800bed50413812c9b9b3c95c6b1dc58b7e5a37ac
Mehdi Mashayekhi <mehdi.mashayekhi.65@gmail.com>
2018-08-21 15:23:00 -0700

    Add files via upload

---
+++ Model_based_RL/controllers.py
@@ -0,0 +1,45 @@
+import numpy as np
+from cost_functions import trajectory_cost_fn
+import time
+
+class Controller():
+	def __init__(self):
+		pass
+
+	# Get the appropriate action(s) for this state(s)
+	def get_action(self, state):
+		pass
+
+
+class RandomController(Controller):
+	def __init__(self, env):
+		""" YOUR CODE HERE """
+		# pass
+		self.ac = env.action_space
+
+	def get_action(self, state):
+		""" YOUR CODE HERE """
+		""" Your code should randomly sample an action uniformly from the action space """
+		# pass
+		return self.ac.sample()
+
+
+class MPCcontroller(Controller):
+	""" Controller built using the MPC method outlined in https://arxiv.org/abs/1708.02596 """
+	def __init__(self, 
+				 env, 
+				 dyn_model, 
+				 horizon=5, 
+				 cost_fn=None, 
+				 num_simulated_paths=10,
+				 ):
+		self.env = env
+		self.dyn_model = dyn_model
+		self.horizon = horizon
+		self.cost_fn = cost_fn
+		self.num_simulated_paths = num_simulated_paths
+
+	def get_action(self, state):
+		""" YOUR CODE HERE """
+		""" Note: be careful to batch your simulations through the model for speed """
+
